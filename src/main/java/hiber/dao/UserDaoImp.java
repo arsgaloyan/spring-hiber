2cf20d808ce74b394be4fdf8248055e822e2ccce
arsgaloyan <139362615+arsgaloyan@users.noreply.github.com>
2023-08-24 05:04:24 +0300

    Task

---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,42 @@
+package hiber.dao;
+
+
+import hiber.model.Car;
+import hiber.model.User;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void add(User user) {
+        sessionFactory.getCurrentSession().persist(user.getCar());
+        sessionFactory.getCurrentSession().persist(user);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public List<User> listUsers() {
+        TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery("from User");
+        return query.getResultList();
+    }
+
+    @Override
+    public User showUser(String model, int series) {
+        String HQL = "FROM User user LEFT JOIN FETCH user.car where user.car.model =: model and user.car.series =: series";
+        User user = sessionFactory.getCurrentSession().createQuery(HQL, User.class).
+                setParameter("model", model)
+                .setParameter("series", series)
+                .uniqueResult();
+        return user;
+    }
+}
